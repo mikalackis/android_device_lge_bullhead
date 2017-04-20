b100df241821925c7979798d782baba8c92aee61
Sava Mikalacki <mikalackis@gmail.com>
2017-04-21 00:12:41 +0200

    Include twrp flags and fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,21 @@ BOARD_HAL_STATIC_LIBRARIES := libdumpstate.bullhead
 
 TARGET_RECOVERY_FSTAB = device/lge/bullhead/fstab.bullhead
 
+#TWRP START
+TW_THEME := portrait_hdpi
+PRODUCT_COPY_FILES += \
+    device/lge/bullhead/twrp.fstab:recovery/root/etc/twrp.fstab
+TW_NO_REBOOT_RECOVERY := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+#TW_OEM_BUILD := true
+#TWRP END
+
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/bullhead
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
